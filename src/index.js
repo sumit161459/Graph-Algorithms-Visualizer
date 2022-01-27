e08f533ae8b0ae4f0b03dc36972711cdfff08b96
“sumit161459” <toppersumitr@iitkgp.ac.in>
2022-01-27 23:06:02 +0530

    rendered the grid with a marker of start and end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PathFindingVisualizer from './PathFindingVisualizer';
+import PathFindingVisualizer from './pathfindingVisualizer/PathFindingVisualizer';
 
 
 
